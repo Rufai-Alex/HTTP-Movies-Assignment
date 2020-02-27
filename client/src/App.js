bdb22b2c95b7f9d35a48dea50b9d7a78f88f312a
Rufai-Alex <oluwasinaalexrufai@gmail.com>
2020-02-27 09:22:49 +0100

    Updatelist

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,8 @@ import { Route } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
-import axios from 'axios';
+import axios from "axios";
+import UpdateMovies from "./Movies/UpDateMovies";
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -28,12 +29,15 @@ const App = () => {
     <>
       <SavedList list={savedList} />
 
-      <Route exact path="/">
+      <Route exact path='/'>
         <MovieList movies={movieList} />
       </Route>
 
-      <Route path="/movies/:id">
+      <Route path='/movies/:id'>
         <Movie addToSavedList={addToSavedList} />
+      </Route>
+      <Route path='/update-movie/:id'>
+        <UpdateMovies />
       </Route>
     </>
   );
---
+++ client/src/Movies/UpDateMovies.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function UpdateMovies() {
+  return <form>inpu</form>;
+}
